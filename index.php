3c00db9b39cc069d1d19d566aa865c2dd0a74dd1
kamotejhon <jhonbustamante011501@gmail.com>
2021-01-10 15:30:58 +0800

    turnitin

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<title> Welcom To my Browse </title>
+</head>
+<style>
+        table, th, td {
+        border: 2px solid black;
+        }
+</style>
+<body>
+    <h1> Dental Clinic Reservation </h1>
+
+    <table style="width:100%">
+    <tr>
+        <th>Time</th>
+        <th>Sunday</th>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wedneday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+        <th>Saturday</th>
+    </tr>   
+    
+    <tr>
+        <th>6:30AM</th>
+    </tr> 
+
+    <tr>
+        <th>8:30AM</th>
+    </tr> 
+
+    <tr>
+        <th>10:30AM</th>
+    </tr> 
+
+    <tr>
+        <th>12:30PM</th>
+    </tr> 
+
+    <tr>
+        <th>3:30PM</th>
+    </tr> 
+
+    <tr>
+        <th>5:30PM</th>
+    </tr> 
+
+    <tr>
+        <th>7:30PM</th>
+    </tr> 
+
+    <tr>
+        <th>9:30PM</th>
+    </tr> 
+
+
+    </table>
+
+    
+
+</body>
+</html>
