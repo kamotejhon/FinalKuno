22b7bffafb7a6811d26c332a529091bfb41f2b85
kamotejhon <jhonbustamante011501@gmail.com>
2021-01-10 15:50:24 +0800

    changes

---
+++ login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title> Welcom To my Browse </title>
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
+</head>
+<body>
+    <div id = "frm">
+        <form action = "process/php" metho = "POST">
+
+            <p>
+                <label>Username</label>
+                <input type = "text" id = "username" name = "username" />
+            </p>
+
+            <p>
+                <label>Password</label>
+                <input type = "password" id = "password" name = "password" />
+            </p>
+
+            <p>
+                <input type = "submit" id = "btn" name = "login" />
+            </p>
+        </form>
+    </div>
+</body>
+</html>
+    
+
